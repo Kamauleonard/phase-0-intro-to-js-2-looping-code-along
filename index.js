10c955ef9ee2d1716af5d44c910cbda4618713e8
Kamauleonard <leonardmwangi2012@gmail.com>
2023-10-04 20:08:58 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solutions in this file
+function writeCards(names) {
+    return names.map(name => `Thank you, ${name}, for the wonderful surprise gift!`);
+}
+
+// Function to count down from a given number
+function countDown(startingNumber) {
+    while (startingNumber >= 0) {
+        console.log(startingNumber);
+        startingNumber--;
+    }
+}
